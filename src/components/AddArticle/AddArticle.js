3756a49747e32ac8b3f6d2498d8d8f38599cf802
Bilal <bsajid173@gmail.com>
2022-09-29 17:43:03 +0530

    Customize Add article modal

--- src/components/AddArticle/AddArticle.js
+++ src/components/AddArticle/AddArticle.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import AddArticleModal from "../UI/AddArticleModal/AddArticleModal";
 // import useInput from "../../hooks/use-input";
 // import AuthContext from "../../store/auth-context";
@@ -12,6 +12,8 @@ const AddNewArticle = (props) => {
   const [imageName, setImageName] = useState("No Image Selected");
   const [fileInputState, setFileInputState] = useState("");
   const [previewSource, setPreviewSource] = useState("");
+  const [paragraphs, setParagraphs] = useState([""]);
+  const [showPreview, setShowPreview] = useState(false);
   const {
     value: enteredTitle,
     hasError: titleHasError,
@@ -66,6 +68,30 @@ const AddNewArticle = (props) => {
       return prev.filter((curtag) => curtag !== tag);
     });
   };
+
+  useEffect(() => {
+    const setParaTimer = setTimeout(() => {
+      setParagraphs((prev) => {
+        console.log(prev);
+        const newPars = enteredContent.split("\\n");
+        prev = newPars.map((par) => {
+          return par.trim();
+        });
+        return prev;
+      });
+    }, 2000);
+
+    return () => {
+      clearInterval(setParaTimer);
+    };
+  }, [enteredContent]);
+
+  const showPreviewHandler = () => {
+    setShowPreview((prev) => {
+      return !prev;
+    });
+  };
+
   const formSubmitHandler = (e) => {
     e.preventDefault();
     console.log(enteredTitle, enteredCategory, enteredContent, tags);
@@ -119,11 +145,11 @@ const AddNewArticle = (props) => {
                 onBlur={titleBlurHandler}
               />
               <textarea
-                rows="8"
+                rows="12"
                 name="content"
                 id="content"
                 className="rounded-sm p-2 my-1 w-full resize-none border-0 outline-none dark:bg-gray-800 dark:border dark:border-gray-600"
-                placeholder="Whats this about?"
+                placeholder="Whats this about? (use \n in your text for a new paragraph and enclose subheadings in {}. Each subheading should be after a newline character)"
                 required
                 value={enteredContent}
                 onChange={contentChangeHandler}
@@ -156,7 +182,7 @@ const AddNewArticle = (props) => {
                 <button
                   disabled={enteredTag === ""}
                   type="button"
-                  className="rounded-sm w-[24%] ml-[1%] p-2 bg-blue-400 hover:bg-blue-500 disabled:hover:bg-blue-400 disabled:opacity-70 disabled:cursor-not-allowed dark:bg-slate-600"
+                  className="rounded-sm w-[24%] ml-[1%] p-2 bg-blue-400 hover:bg-blue-500 disabled:hover:bg-blue-400 disabled:opacity-70 disabled:cursor-not-allowed dark:bg-slate-600  dark:hover:bg-slate-700"
                   onClick={addTagHandler}
                 >
                   Add
@@ -185,7 +211,7 @@ const AddNewArticle = (props) => {
               ></input>
               <label
                 for="imagepick"
-                className="p-1 px-3 bg-blue-400 cursor-pointer rounded-sm hover:bg-blue-500 mr-3 dark:bg-slate-600"
+                className="p-1 px-3 bg-blue-400 cursor-pointer rounded-sm hover:bg-blue-500 mr-3 dark:bg-slate-600  dark:hover:bg-slate-700"
                 type="button"
               >
                 Choose Image
@@ -193,68 +219,92 @@ const AddNewArticle = (props) => {
               <span>{imageName}</span>
             </div>
             <button
-              className="p-1 px-3 my-3 bg-blue-400 cursor-pointer rounded-sm w-full hover:bg-blue-500 dark:bg-slate-600"
+              className="p-1 px-3 my-3 bg-blue-400 cursor-pointer rounded-sm w-full hover:bg-blue-500 dark:bg-slate-600 dark:hover:bg-slate-700"
               type="submit"
             >
               Submit
             </button>
           </form>
-          <h1 className="font-bold text-2xl">Preview</h1>
-          <div className="border border-gray-300 dark:border-gray-500 flex flex-col p-3 bg-white dark:bg-[#07244C] dark:text-white my-3 first:mt-0 rounded-sm">
-            <div className="flex flex-wrap justify-center mb-2">
-              {/*use cloudinary image component for image*/}
-              <div className="rounded-full h-16 w-16 flex flex-wrap bg-gray-500 dark:bg-white">
-                <img src={image} className="" alt="img"></img>
-              </div>
-              <div className="ml-2 mr-auto flex flex-wrap flex-col items-start justify-start">
-                <h2 className="font-bold">Bilal Sajid</h2>
-                <span>Today</span>
-              </div>
-              <div>
-                <span>
-                  <i class="fa-regular fa-star"></i>
-                  {/* <i class="fa-solid fa-star"></i> */}
-                </span>
-              </div>
-            </div>
-            <div className="">
-              <h1 className="text-2xl font-bold">{enteredTitle}</h1>
-              <p>{enteredContent}</p>
-              <div className="flex flex-wrap mt-3">
-                {previewSource ? (
-                  <img
-                    className="w-full h-48"
-                    src={previewSource}
-                    alt="preview_img"
-                  ></img>
-                ) : (
-                  "No Image Selected"
-                )}
-              </div>
-              <div className="flex flex-wrap mt-2">
-                {tags.map((tag) => {
-                  return (
-                    <div className="transition-all cursor-pointer rounded-sm w-fit p-1 mx-2 hover:bg-red-200 first:ml-0 dark:hover:text-black">
-                      <span>#{tag}</span>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="flex flex-wrap mt-3">
-                <div className="flex flex-wrap transition-all cursor-pointer rounded-sm w-fit mx-2 ml-0  dark:hover:text-black">
-                  <span className="p-1 px-2 border border-rose-400 rounded-sm  hover:bg-gray-100">
-                    <i class="fas mr-2 fa-light fa-message"></i>{" "}
-                    {props.comments} comments
-                  </span>
+          {/* <h1 className="font-bold text-2xl">Preview</h1> */}
+          <button
+            onClick={showPreviewHandler}
+            className="p-2 bg-blue-300 rounded-sm font-semibold hover:bg-blue-400 dark:bg-gray-600  dark:hover:bg-slate-700"
+          >
+            {showPreview ? "Hide Preview" : "Show Preview"}
+          </button>
+          {showPreview && (
+            <div className="border border-gray-300 dark:border-gray-500 flex flex-col p-3 bg-white dark:bg-[#07244C] dark:text-white my-3 first:mt-0 rounded-sm">
+              <div className="flex flex-wrap justify-center mb-2">
+                {/*use cloudinary image component for image*/}
+                <div className="rounded-full h-16 w-16 flex flex-wrap bg-gray-500 dark:bg-white">
+                  <img src={image} className="" alt="img"></img>
                 </div>
-                <div className="flex flex-wrap transition-all cursor-pointer rounded-sm w-fit mx-2  dark:hover:text-black">
-                  <span className="p-1 px-2 border border-rose-400 rounded-sm  hover:bg-gray-100">
-                    <i class="mr-2 fa-solid fa-heart"></i> {props.likes} likes
+                <div className="ml-2 mr-auto flex flex-wrap flex-col items-start justify-start">
+                  <h2 className="font-bold">Bilal Sajid</h2>
+                  <span>Today</span>
+                </div>
+                <div>
+                  <span>
+                    <i class="fa-regular fa-star"></i>
+                    {/* <i class="fa-solid fa-star"></i> */}
                   </span>
                 </div>
               </div>
+              <div className="">
+                <h1 className="text-2xl font-bold mb-2">{enteredTitle}</h1>
+                <div className="flex flex-wrap mt-3 mb-3">
+                  {previewSource ? (
+                    <img
+                      className="w-full h-48"
+                      src={previewSource}
+                      alt="preview_img"
+                    ></img>
+                  ) : (
+                    "No Image Selected"
+                  )}
+                </div>
+                <div>
+                  {paragraphs.map((para) => {
+                    let heading = "";
+                    if (para[0] === "{") {
+                      heading = para.split("}")[0].substring(1);
+                      para = para.split("}")[1];
+                    }
+                    return (
+                      <>
+                        {heading !== "" && (
+                          <h2 className="font-semibold">{heading}</h2>
+                        )}
+                        <p className="mb-2">{para}</p>
+                      </>
+                    );
+                  })}
+                </div>
+                <div className="flex flex-wrap mt-2">
+                  {tags.map((tag) => {
+                    return (
+                      <div className="transition-all cursor-pointer rounded-sm w-fit p-1 mx-2 hover:bg-red-200 first:ml-0 dark:hover:text-black">
+                        <span>#{tag}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="flex flex-wrap mt-3">
+                  <div className="flex flex-wrap transition-all cursor-pointer rounded-sm w-fit mx-2 ml-0  dark:hover:text-black">
+                    <span className="p-1 px-2 border border-rose-400 rounded-sm  hover:bg-gray-100">
+                      <i class="fas mr-2 fa-light fa-message"></i>{" "}
+                      {props.comments} comments
+                    </span>
+                  </div>
+                  <div className="flex flex-wrap transition-all cursor-pointer rounded-sm w-fit mx-2  dark:hover:text-black">
+                    <span className="p-1 px-2 border border-rose-400 rounded-sm  hover:bg-gray-100">
+                      <i class="mr-2 fa-solid fa-heart"></i> {props.likes} likes
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </Fragment>
