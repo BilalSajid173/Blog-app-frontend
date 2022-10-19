bc71e5125abeb7f799b8bbe8378219221299854c
Bilal <bsajid173@gmail.com>
2022-10-20 02:02:10 +0530

    add comments reducers

--- src/store/comments.js
+++ src/store/comments.js
@@ -7,7 +7,29 @@ const initialCommentsState = {
 const commentsSlice = createSlice({
   name: "comments",
   initialState: initialCommentsState,
-  reducers: {},
+  reducers: {
+    addComments(state, action) {
+      state.comments = action.payload.comments;
+    },
+    addNewComment(state, action) {
+      state.comments.push(action.payload.comment);
+    },
+    deleteComment(state, action) {
+      state.comments = state.comments.filter((comment) => {
+        return comment.id !== action.payload.id;
+      });
+    },
+    updateComment(state, action) {
+      let comment = state.comments.filter((comment) => {
+        return comment.id === action.payload.id;
+      });
+      comment.content = action.payload.content;
+      state.comments = state.comments.filter((comment) => {
+        return comment.id !== action.payload.id;
+      });
+      state.comments.push(comment);
+    },
+  },
 });
 
 export const commentsActions = commentsSlice.actions;
