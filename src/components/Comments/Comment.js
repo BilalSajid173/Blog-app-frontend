6492cf32e89845688c6a5570193330f66d5575b2
Bilal <bsajid173@gmail.com>
2022-10-19 11:55:17 +0530

    improvement in commens modal and stuff

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -8,6 +8,56 @@ import CloseIcon from "@mui/icons-material/Close";
 import OptionsMenu from "./CommentOptions";
 import { TextField } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+
+const dummycomments = [
+  {
+    name: "Bilal",
+    created_at: "12 hours ago",
+    content: "lorem ipsum dolor sit amet hahahahahaha",
+    likes: 120,
+    dislikes: 20,
+  },
+  {
+    name: "Rashid",
+    created_at: "7 hours ago",
+    content: "lorem ipsum dolor sit amet hahahahahaha",
+    likes: 80,
+    dislikes: 25,
+  },
+  {
+    name: "Aatir",
+    created_at: "5 hours ago",
+    content: "lorem ipsum dolor sit amet hahahahahaha",
+    likes: 80,
+    dislikes: 25,
+  },
+  {
+    name: "Anzal",
+    created_at: "10 hours ago",
+    content:
+      "lorem ipsum dolor sit amet hahahahahaha this is a comment to see if this is a comment",
+    likes: 80,
+    dislikes: 25,
+  },
+  {
+    name: "Faiz",
+    created_at: "3 hours ago",
+    content:
+      "lorem ipsum dolor sit amet hello there lololo hahahahahaha this is a comment to see if this is a comment",
+    likes: 9,
+    dislikes: 25,
+  },
+  {
+    name: "Ijlal",
+    created_at: "6 hours ago",
+    content:
+      "lorem ipsum dolor sit amet hahahahahaha this fkjk fjkdjf jfsfkjdkvj is a comment to see if this is a comment",
+    likes: 50,
+    dislikes: 2,
+  },
+];
 
 const ViewComments = (props) => {
   const [showOptions, setShowOptions] = useState(false);
@@ -36,7 +86,7 @@ const ViewComments = (props) => {
     <Fragment>
       <div className="p-2 dark:text-white">
         <div className="flex flex-wrap justify-center">
-          <span className="mr-auto">Comments 50</span>
+          <span className="mr-auto">Comments {dummycomments.length}</span>
           <div className="flex flex-wrap justify-center items-center">
             <OptionsMenu className="cursor-pointer" />
             <CloseIcon
@@ -49,7 +99,7 @@ const ViewComments = (props) => {
           <img
             src={image}
             alt="user"
-            className="w-12 h-12 rounded-full bg-white mr-4"
+            className="w-10 h-10 rounded-full bg-white mr-4"
           />
           <div className="w-[80%] sm:w-[85%]">
             <ThemeProvider theme={darkTheme}>
@@ -66,19 +116,52 @@ const ViewComments = (props) => {
               <div className="mt-2 flex flex-wrap ">
                 <button
                   onClick={closeOptionsDiv}
-                  className="p-2 ml-auto bg-gray-700 rounded-sm mr-2"
+                  className="p-2 ml-auto bg-gray-300 dark:bg-gray-700 rounded-sm mr-2"
                 >
                   CANCEL
                 </button>
                 <button
                   disabled={isdisabled ? true : false}
-                  className="p-2 bg-gray-700 rounded-sm disabled:bg-zinc-700 disabled:text-gray-500"
+                  className="p-2 bg-gray-300 dark:bg-gray-700 rounded-sm disabled:bg-slate-300 disabled:opacity-40 disabled:dark:bg-zinc-700 disabled:dark:text-gray-500"
                 >
                   COMMENT
                 </button>
               </div>
             )}
           </div>
+        </div>
+        <div className="flex flex-wrap mt-8">
+          {dummycomments.map((comment) => {
+            return (
+              <div className="flex flex-wrap mb-4">
+                <img
+                  src={image}
+                  alt="user"
+                  className="w-10 h-10 rounded-full bg-white mr-4"
+                />
+                <div className="flex flex-wrap w-[80%]">
+                  <div className="flex flex-wrap items-center">
+                    <span className="font mr-2">{comment.name}</span>
+                    <span className=" font-thin text-sm">
+                      {comment.created_at}
+                    </span>
+                  </div>
+                  <div className="">
+                    <p>{comment.content}</p>
+                  </div>
+                  <div className="mt-2">
+                    <span className="mr-4">
+                      <ThumbUpOffAltIcon className="mr-2" /> {comment.likes}
+                    </span>
+                    <span>
+                      <ThumbDownOffAltIcon className="mr-2" />{" "}
+                      {comment.dislikes}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </Fragment>
